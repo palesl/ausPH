06735651ccbb64af57efbaf7605a1559aece4bd3
palesl <patrick.leslie@anu.edu.au>
2024-08-19 16:11:12 +1000

    get schools

--- R/getSchools.R
+++ R/getSchools.R
@@ -1,22 +1,22 @@
-#' Data on all listed Qualifications
+#' Data on all listed Secondary Schools
 #' @return a list
 #' @export
 #'
-#' @examples getQualifications()
-getQualifications<-function(){
+#' @examples getSchools()
+getSchools<-function(){
   url<- "https://handbookapi.aph.gov.au/api/individuals"
   dat<-rjson::fromJSON(file=url)$value
 
-  result_df <- data.frame(PHID = character(), Qualification = character(), stringsAsFactors = FALSE)
+  result_df <- data.frame(PHID = character(), School = character(), stringsAsFactors = FALSE)
 
   # Loop through each element of the list
   for (element in dat) {
     # Extract PHID and Occupations
     phid <- element[["PHID"]]
-    quals <- element[["Qualifications"]]
+    quals <- element[["SecondarySchool"]]
 
     # Create a temporary data frame for the current element
-    temp_df <- data.frame(PHID = rep(phid, length(quals)), Qualification = quals, stringsAsFactors = FALSE)
+    temp_df <- data.frame(PHID = rep(phid, length(quals)), secondary_school = quals, stringsAsFactors = FALSE)
 
     # Append the temporary data frame to the result data frame
     result_df <- rbind(result_df, temp_df)
